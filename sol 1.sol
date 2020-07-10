a34e08ed6738128687f13d03697b431fcb6710d5
CubeACloud <cubeacloud@gmail.com>
2020-07-11 00:41:57 +0500

    create a contract to get & set value int & string

--- sol 1.sol
+++ sol 1.sol
@@ -1,1 +1,26 @@
-pragma 
+pragma solidity ^0.4.24;
+
+contract myContract {
+    
+    string value;
+    int number;
+    constructor () public {
+        value  ;
+        number ;
+    }
+    
+    
+     function getBoth() public view returns(int , string) {
+        return  (number , value);
+        
+    }
+    
+    function setValue(string _value) public {
+        value = _value;
+    }
+    
+      function setNumber(int _number) public  {
+        number = _number;
+    }
+
+}
